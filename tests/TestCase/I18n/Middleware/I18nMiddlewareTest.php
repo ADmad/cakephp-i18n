9267904bf9cb7d8a83f5acbf770a7054222e4205
ADmad <admad.coder@gmail.com>
2016-07-12 18:51:16 +0530

    Add I18nMiddleware.

---
+++ tests/TestCase/I18n/Middleware/I18nMiddlewareTest.php
@@ -0,0 +1,98 @@
+<?php
+namespace ADmad\I18n\Test\TestCase\I18n\Middleware;
+
+use ADmad\I18n\I18n\Middleware\I18nMiddleware;
+use Cake\Http\ServerRequestFactory;
+use Cake\I18n\I18n;
+use Cake\TestSuite\TestCase;
+use Locale;
+use Zend\Diactoros\Request;
+use Zend\Diactoros\Response;
+
+/**
+ * I18nMiddleware test.
+ */
+class I18nMiddlewareTest extends TestCase
+{
+    /**
+     * setup.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->locale = Locale::getDefault();
+
+        $this->config = [
+            'defaultLanguage' => 'fr',
+            'languages' => ['fr', 'en'],
+        ];
+
+        $this->request = ServerRequestFactory::fromGlobals([
+            'REQUEST_URI' => '/',
+        ]);
+        $this->request = $this->request->withAttribute('webroot', '/');
+        $this->response = new Response();
+        $this->next = function ($req, $res) {
+            return $res;
+        };
+    }
+
+    /**
+     * Resets the default locale.
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        Locale::setDefault($this->locale);
+        unset($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+    }
+
+    /**
+     * testRedirectionFromSiteRoot.
+     *
+     * @return void
+     */
+    public function testRedirectionFromSiteRoot()
+    {
+        $middleware = new I18nMiddleware($this->config);
+        $response = $middleware($this->request, $this->response, $this->next);
+
+        $headers = $response->getHeaders();
+        $this->assertEquals('/fr', $headers['location'][0]);
+        $this->assertEquals(301, $response->getStatusCode());
+
+        $_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'en-US,en;q=0.8,es;q=0.6,da;q=0.4';
+        $request = ServerRequestFactory::fromGlobals([
+            'REQUEST_URI' => '/'
+        ]);
+        $middleware = new I18nMiddleware($this->config + ['detectLanguage' => true]);
+        $response = $middleware($request, $this->response, $this->next);
+
+        $headers = $response->getHeaders();
+        $this->assertEquals('/en', $headers['location'][0]);
+        $this->assertEquals(302, $response->getStatusCode());
+    }
+
+    /**
+     * testLocaleSetting.
+     *
+     * @return void
+     */
+    public function testLocaleSetting()
+    {
+        $request = ServerRequestFactory::fromGlobals([
+            'REQUEST_URI' => '/fr',
+        ]);
+        $request = $request->withAttribute('params', ['lang' => 'fr']);
+        $middleware = new I18nMiddleware($this->config);
+        $response = $middleware($request, $this->response, $this->next);
+
+        $this->assertEquals('fr', I18n::locale());
+    }
+}
