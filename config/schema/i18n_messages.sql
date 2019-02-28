87ee45dc0a991ce4209a7f90f7766488f796649f
ADmad <admad.coder@gmail.com>
2019-02-28 12:21:13 +0530

    Update SQL for messages table.

--- config/schema/i18n_messages.sql
+++ config/schema/i18n_messages.sql
@@ -1,13 +1,14 @@
-CREATE TABLE `i18n_messages` (
+CREATE TABLE `translations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `domain` varchar(100) NOT NULL,
   `locale` varchar(5) NOT NULL,
   `singular` varchar(255) NOT NULL,
-  `plural` varchar(255) NOT NULL,
-  `context` varchar(50) NOT NULL,
-  `value_0` varchar(255) NOT NULL,
-  `value_1` varchar(255) NOT NULL,
-  `value_2` varchar(255) NOT NULL,
+  `plural` varchar(255) DEFAULT NULL,
+  `context` varchar(50) DEFAULT NULL,
+  `value_0` varchar(255) DEFAULT NULL,
+  `value_1` varchar(255) DEFAULT NULL,
+  `value_2` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `domain_locale_singular` (`domain`,`locale`,`singular`),
   KEY `domain_locale` (`domain`,`locale`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
